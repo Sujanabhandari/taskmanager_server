4c4f29ed27671f57eb56b96fc8a8a9f9c0064955
Sujana Bhandari <sujanabhandari2021@gmail.com>
2023-01-25 10:39:13 +0100

    Initial setup

---
+++ app.ts
@@ -0,0 +1,35 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import * as cors from 'cors';
+import ErrorResponse from './src/utils/ErrorResponse';
+
+const app = express();
+app.use(express.json());
+
+app.use(cors.default({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }))
+const port = process.env.port || 3333;
+
+app.use(function(err: ErrorResponse, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.error(err.stack);
+  // render the error page
+  res.status(err.statusCode || 500);
+  res.send({
+    error: err.message
+  });
+});
+
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
