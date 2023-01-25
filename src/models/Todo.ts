8608c6ed65202e28928f319dc2a08ddc89ccb341
Sujana Bhandari <sujanabhandari2021@gmail.com>
2023-01-25 14:30:21 +0100

    Create Todo and TodoList schema

---
+++ src/models/Todo.ts
@@ -0,0 +1,22 @@
+import * as mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    done: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    todolistId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'todolist',
+      required: true,
+      onDelete: 'CASCADE',
+    },
+  });
+  
+const Todo = mongoose.model("Todo", todoSchema);
+export default Todo;
